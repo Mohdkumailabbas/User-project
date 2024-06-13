f8ecb21c600ca11feff386e48f780acbf8202d7c
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2024-06-13 23:45:00 +0530

    zod added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app= express();
 const usermodel=require('./models/user');
 const user = require("./models/user");
+const { z } = require("zod");
+
 app.set('view engine',"ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -10,16 +12,25 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get("/",(req,res)=>{
     res.render("index")
 })
-app.post("/create", async(req,res)=>{
-    let {name,email,image}=req.body
-    let createduser =  await  usermodel.create({
-        name,
-        email,
-        image
-
-    })
-    res.redirect("/read");
+const userSchema=z.object({
+    name: z.string().min(3, { message: "Name is required" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    image: z.string().url({ message: "Invalid image URL" })
 })
+app.post("/create", async (req, res) => {
+    try {
+        const validatedData = userSchema.parse(req.body);
+        let { name, email, image } = validatedData;
+        let createduser = await usermodel.create({
+            name,
+            email,
+            image
+        });
+        res.redirect("/read");
+    } catch (error) {
+        res.status(400).send(error.errors);
+    }
+});
 app.get("/read", async(req,res)=>{
     let users= await usermodel.find();
     res.render("read",{users})
